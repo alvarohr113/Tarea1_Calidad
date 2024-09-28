598aa39a3063c628bd9f0d9a2d54e1ff12a0abf2
alvarohr113 <alvaro201102@gmail.com>
2024-09-28 16:16:21 +0200

    mi primer commit

---
+++ src/Grupo_L2/aplicacion.java
@@ -0,0 +1,12 @@
+package Grupo_L2;
+
+public class aplicacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Comienzo de practica");
+		System.out.println("Comienzo de practica2");
+
+	}
+
+}
